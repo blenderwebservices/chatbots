0f2dfc8c2d4f939fda335ce2b3c16809a8706be0
Jorge García <jorge@aprendible.com>
2024-11-08 21:32:38 -0500

    08 - Chatbots list

---
+++ resources/js/Components/Chatbots/ChatbotsListItem.vue
@@ -0,0 +1,41 @@
+<script setup>
+import PrimaryButton from '@/Components/PrimaryButton.vue'
+import SecondaryButton from '@/Components/SecondaryButton.vue'
+import DangerButton from '@/Components/DangerButton.vue'
+
+defineProps({
+  chatbot: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <section
+    class="border-gray-200 p-5 dark:border-gray-700"
+    :aria-label="chatbot.name"
+  >
+    <div
+      class="flex flex-col md:flex-row md:items-center md:justify-between"
+    >
+      <div>
+        <div class="text-gray-800 dark:text-gray-200">
+          {{ chatbot.name }}
+        </div>
+        <div
+          class="text-sm text-gray-500 dark:text-gray-400"
+        >
+          {{ chatbot.created_at }}
+        </div>
+      </div>
+      <div class="mt-4 flex space-x-2 md:mt-0">
+        <PrimaryButton>View</PrimaryButton>
+        <SecondaryButton>Edit</SecondaryButton>
+        <DangerButton>Delete</DangerButton>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
---
+++ resources/js/Pages/Chatbots/Index.vue
@@ -0,0 +1,37 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue'
+import ChatbotsListItem from '@/Components/Chatbots/ChatbotsListItem.vue'
+
+defineProps({
+  chatbots: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <AppLayout title="Dashboard">
+    <template #header>
+      <h2
+        class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
+      >
+        Chatbots
+      </h2>
+    </template>
+
+    <section class="py-12" aria-label="Chatbots List">
+      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div
+          class="divide-y overflow-hidden bg-white shadow-xl sm:rounded-lg dark:bg-gray-800"
+        >
+          <ChatbotsListItem
+            v-for="chatbot in chatbots"
+            :key="chatbot.id"
+            :chatbot="chatbot"
+          />
+        </div>
+      </div>
+    </section>
+  </AppLayout>
+</template>
