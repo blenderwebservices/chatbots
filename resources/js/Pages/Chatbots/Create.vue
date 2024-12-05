1107fa00ffc98d0fb770e8d2dd4b70ebc15b2a9f
Jorge García <jorge@aprendible.com>
2024-12-04 19:32:49 -0500

    18 - Chatbot Create

---
+++ resources/js/Pages/Chatbots/Create.vue
@@ -0,0 +1,67 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue'
+import FormSection from '@/Components/FormSection.vue'
+import PrimaryButton from '@/Components/PrimaryButton.vue'
+import ActionMessage from '@/Components/ActionMessage.vue'
+import ChatbotsForm from '@/Components/Chatbots/ChatbotsForm.vue'
+import { useForm } from '@inertiajs/vue3'
+
+const props = defineProps({
+  chatbot: {
+    type: Object,
+    required: true,
+  },
+})
+
+const form = useForm({
+  name: props.chatbot.name,
+  system_prompt: props.chatbot.system_prompt,
+  model: props.chatbot.model,
+  temperature: String(props.chatbot.temperature),
+})
+
+const handleSubmit = () => {
+  form.post(route('chatbots.store'), {
+    preserveScroll: true,
+  })
+}
+</script>
+
+<template>
+  <AppLayout title="Nuevo Chatbot">
+    <template #header>
+      <h1
+        class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
+      >
+        Nuevo Chatbot
+      </h1>
+    </template>
+    <section class="py-6" aria-label="Editar Chatbot">
+      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <FormSection @submitted="handleSubmit">
+          <template #title> Crear Chatbot</template>
+          <template #description>
+            Crea un nuevo chatbot para tu aplicación.
+          </template>
+          <template #form>
+            <ChatbotsForm :form="form" />
+          </template>
+          <template #actions>
+            <PrimaryButton
+              aria-label="Crear Chatbot"
+              :class="{
+                'cursor-not-allowed opacity-50':
+                  form.processing,
+              }"
+              :disabled="form.processing"
+            >
+              Crear Chatbot
+            </PrimaryButton>
+          </template>
+        </FormSection>
+      </div>
+    </section>
+  </AppLayout>
+</template>
+
+<style scoped></style>
