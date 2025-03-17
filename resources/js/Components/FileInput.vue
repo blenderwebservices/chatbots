b36d3343a3dda237b4866c47ba27124478903d69
Jorge García <jorge@aprendible.com>
2025-03-17 11:37:41 -0500

    28 - File input design

--- resources/js/Components/FileInput.vue
+++ resources/js/Components/FileInput.vue
@@ -22,7 +22,7 @@ defineExpose({ focus: () => input.value.focus() })
   <input
     ref="input"
     type="file"
-    class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
+    class="rounded-md border border-gray-300 p-2 shadow-sm file:mr-2 file:cursor-pointer file:rounded file:border-none file:bg-gray-200 file:px-3 file:py-1 file:text-gray-800 focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 file:dark:bg-gray-700 file:dark:text-white dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
     @input="
       $emit('update:modelValue', $event.target.files[0])
     "
