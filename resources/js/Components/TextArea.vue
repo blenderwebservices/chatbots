dc802c9548674a34aadd1cc8cb7bb8cbca4a0cdf
Jorge García <jorge@aprendible.com>
2025-07-08 22:07:20 -0500

    43 - TextArea shift enter

--- resources/js/Components/TextArea.vue
+++ resources/js/Components/TextArea.vue
@@ -18,6 +18,16 @@ onMounted(() => {
 })
 
 defineExpose({ focus: () => textarea.value.focus() })
+
+const handleEnterPressed = event => {
+  if (event.shiftKey) {
+    return
+  }
+
+  event.preventDefault()
+
+  event.target.form.dispatchEvent(new Event('submit'))
+}
 </script>
 
 <template>
@@ -26,5 +36,6 @@ defineExpose({ focus: () => textarea.value.focus() })
     class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
+    @keydown.enter="handleEnterPressed"
   />
 </template>
