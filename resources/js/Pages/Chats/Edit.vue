720eb69479d75529dedcace3914f835a18a374a7
Jorge García <jorge@aprendible.com>
2025-10-27 20:13:05 -0500

    56 - Auto-scroll del chat al enviar nuevos mensajes en Vue

--- resources/js/Pages/Chats/Edit.vue
+++ resources/js/Pages/Chats/Edit.vue
@@ -3,19 +3,28 @@ import AppLayout from '@/Layouts/AppLayout.vue'
 import ChatInput from '@/Components/Chats/ChatInput.vue'
 import ChatMessage from '@/Components/Chats/ChatMessage.vue'
 import ChatTitle from '@/Components/Chats/ChatTitle.vue'
+import { nextTick, useTemplateRef } from 'vue'
 
 const props = defineProps({
   chat: Object,
   messages: Array,
 })
-
-const sendMessage = message => {
+const chatContainer = useTemplateRef('chatContainer')
+const scrollToBottom = () => {
+  chatContainer.value.scrollTo({
+    top: chatContainer.value.scrollHeight,
+    behavior: 'smooth',
+  })
+}
+const sendMessage = async message => {
   props.messages.push({
     content: message,
     role: 'user',
     created_at: new Date(),
   })
+  await nextTick()
   // scroll
+  scrollToBottom()
   // enviar mensaje al chat
   console.log(message)
 }
@@ -30,6 +39,7 @@ const sendMessage = message => {
       class="flex h-[calc(100vh-140px)] flex-col bg-gray-100 dark:bg-gray-900"
     >
       <div
+        ref="chatContainer"
         class="mx-auto max-w-7xl flex-1 overflow-y-auto p-6"
       >
         <ChatMessage
